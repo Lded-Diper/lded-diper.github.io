d3e11d854cc43fc868d81bd79eaecd07611aefd5
Oliver Retzloff <300082482@fvtc.edu>
2026-01-28 13:11:12 -0600

    Added welcome div

---
+++ site.js
@@ -0,0 +1,20 @@
+const hours = new Date().getHours() // get the current hour
+
+const isMorning = hours >= 4 && hours < 12 // is it morning?
+const isAfternoon = hours >= 12 && hours < 17 // is it afternoon?
+const isEvening = hours >= 17 || hours < 4 // is it evening?
+
+const thing = document.querySelector('#welcome')
+
+if(isMorning)
+{
+    thing.textContent = 'Good Morning'
+}
+if(isAfternoon)
+{
+    thing.textContent = 'Good Afternoon'
+}
+if(isEvening)
+{
+    thing.textContent = 'Good Evening'
+}
