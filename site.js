380768743e70016e6afa90f0b048058202e53646
Oliver Retzloff <300082482@fvtc.edu>
2026-02-18 16:58:39 -0600

    Added carousel

--- site.js
+++ site.js
@@ -1,5 +1,27 @@
 const hours = new Date().getHours() // get the current hour
 
+const urls = [
+    'https://images.pexels.com/photos/1454360/pexels-photo-1454360.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
+    'https://images.pexels.com/photos/933964/pexels-photo-933964.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
+    'https://images.pexels.com/photos/267885/pexels-photo-267885.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
+    'https://images.pexels.com/photos/1251861/pexels-photo-1251861.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
+    'https://images.pexels.com/photos/1370296/pexels-photo-1370296.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1'
+].map(url => { (new Image()).src = url; return url })
+
+const images = document.querySelectorAll('#carousel img')
+
+let currentImage = 0
+const showImages = () => {
+    const offset = currentImage % urls.length
+    images.forEach((image, index) => {
+        const imageIndex = (index + offset + urls.length) % urls.length
+        image.src = urls[imageIndex]
+    })
+}
+
+showImages()
+
+
 const isMorning = hours >= 4 && hours < 12 // is it morning?
 const isAfternoon = hours >= 12 && hours < 17 // is it afternoon?
 const isEvening = hours >= 17 || hours < 4 // is it evening?
@@ -20,3 +42,20 @@ if(isEvening)
 }
 
 localStorage.setItem("It's a secret to everybody.", 'they nerfed my character in the game i play and i am not happy about it')
+
+const next = document.querySelector('#next')
+next.addEventListener('click', () => {
+    currentImage++
+    showImages()
+})
+
+const prev = document.querySelector('#prev')
+prev.addEventListener('click', () => {
+    currentImage--
+    showImages()
+})
+
+setInterval(() => {
+    currentImage++
+    showImages()
+}, 5000)
